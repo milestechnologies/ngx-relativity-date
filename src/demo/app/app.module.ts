37fee6634c12da3c81d79526b3fa96ca7849aa27
GAH3R0 <gah3r0@gmail.com>
2019-07-10 15:25:23 -0400

    Created a simple demo component for retrieving the current date. Also reinstalled packages necessary for code snippets and their styling.

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -3,21 +3,39 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HighlightModule } from 'ngx-highlightjs';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo.component';
 import { TestComponent } from './components/test.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SimpleDateRetrievalComponent } from './components/simple-date-retrieval.component';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
+
+export function hljsLanguages(): any {
+    return [
+        { name: 'typescript', func: typescript },
+        { name: 'xml', func: xml }
+    ];
+}
 
 @NgModule({
     bootstrap: [AppComponent],
-    declarations: [AppComponent, DemoComponent, TestComponent],
+    declarations: [
+        AppComponent,
+        DemoComponent,
+        TestComponent,
+        SimpleDateRetrievalComponent
+    ],
     imports: [
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HighlightModule.forRoot({
+            languages: hljsLanguages
+        })
     ],
     providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
---
+++ src/demo/app/components/simple-date-retrieval.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+    selector: 'app-test',
+    templateUrl: './simple-date-retrieval.component.html'
+})
+export class SimpleDateRetrievalComponent {
+    dateRetrieval: string;
+    constructor() {}
+
+    ngOnInit(): void {
+        this.dateRetrieval = `
+moment(new Date()).format('MM/DD/YYYY')`;
+    }
+
+    alertMessage(): void {
+        alert(
+            'Todays date is ' + moment(new Date()).format('MM/DD/YYYY') + '!'
+        );
+    }
+}
