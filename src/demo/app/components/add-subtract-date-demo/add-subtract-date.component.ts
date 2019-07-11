87bef494036478d1e07e5e3b946b19f605b50630
Mark Valeriani <markv2311@gmail.com>
2019-07-11 16:27:19 -0400

    Makes sure for valid inputs and Forms

--- src/demo/app/components/add-subtract-date-demo/add-subtract-date.component.ts
+++ src/demo/app/components/add-subtract-date-demo/add-subtract-date.component.ts
@@ -32,20 +32,21 @@ export class AddSubtractDateComponent {
         const which = this.addSubSelect.value.key1;
         // this is the parts in add
         const part = this.enumForm.value.key2;
-        console.log(part);
         // this is the amount
         const quantity = this.quantityForm.value.key3;
-        console.log(quantity);
-
+        if (quantity > 100 || quantity < 0) {
+                alert('Pick a number less than 100 and greater than 0');
+                this.quantityForm.value.key3 = '';
+                this.addSubSelect.value.key1 = '';
+                this.enumForm.value.key2 = '';
+                return;
+        }
         if (which === 'add') {
-            console.log(quantity, part);
             this.currentDate.mtDate.add(quantity, part);
-            console.log(this.currentDate.mtDate);
         }
 
         if (which === 'sub') {
             this.currentDate.mtDate.subtract(quantity, part);
-            console.log(this.currentDate.mtDate);
         }
     }
     setDate(): void {
