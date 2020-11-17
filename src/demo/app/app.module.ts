3549c0bc6164ac44959f42dd0486f3745c7b04df
John Graham <jgraham@4miles.com>
2020-11-17 15:02:20 -0500

    upgrade(Angular 10)

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -3,32 +3,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { HighlightModule } from 'ngx-highlightjs';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test.component';
 import { AppRoutingModule } from './app-routing.module';
-import {
-    NgxRelativityDateModule,
-    IDateModuleConfiguration
-} from '../../../dist';
 
-const myCustomConfiguration: IDateModuleConfiguration = {}; // TODO
-
-import { SimpleDateRetrievalComponent } from './components/simple-date-retrieval.component';
 import typescript from 'highlight.js/lib/languages/typescript';
 import xml from 'highlight.js/lib/languages/xml';
+import { SimpleDateRetrievalComponent } from './components/simple-date-retrieval.component';
 import { AddSubtractDateComponent } from './components/add-subtract-date-demo/add-subtract-date.component';
 import { TimeDescriptionComponent } from './components/time-description-demo/time-description.component';
 import { ToFromComponent } from './components/to-from-demo/to-from.component';
 import { FormatSwitchComponent } from './components/format-switch-demo/format-switch-demo.component';
 import { HolidayDemoComponent } from './components/holiday-demo/holiday-demo.component';
-
-export function hljsLanguages(): any {
-    return [
-        { name: 'typescript', func: typescript },
-        { name: 'xml', func: xml }
-    ];
-}
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -48,11 +35,19 @@ export function hljsLanguages(): any {
         AppRoutingModule,
         FormsModule,
         ReactiveFormsModule,
-        HighlightModule.forRoot({
-            languages: hljsLanguages
-        }),
-        NgxRelativityDateModule.forRoot()
+        HighlightModule
     ],
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        {
+            provide: HIGHLIGHT_OPTIONS,
+            useValue: {
+                languages: {
+                    typescript: typescript,
+                    xml: xml
+                }
+            }
+        }
+    ]
 })
 export class AppModule {}
